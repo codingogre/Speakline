6fdb11e0d0794fdcb512bd8329f0b3210096b3bd
Shawn Hooton <shooton@gopivotal.com>
2014-02-09 14:13:24 -0500

    Added the ability to select voices.

--- SpeakLine/SpeakLIneAppDelegate.h
+++ SpeakLine/SpeakLIneAppDelegate.h
@@ -8,13 +8,17 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface SpeakLIneAppDelegate : NSObject <NSApplicationDelegate>
+@interface SpeakLIneAppDelegate : NSObject <NSApplicationDelegate, NSSpeechSynthesizerDelegate>
 {
     NSSpeechSynthesizer *_speechSynth;
+    NSArray *_voices;
 }
 
 @property (assign) IBOutlet NSWindow *window;
 @property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSButton *stopButton;
+@property (weak) IBOutlet NSButton *speakButton;
+@property (weak) IBOutlet NSTableView *tableView;
 
 - (IBAction)stopIt:(id)sender;
 - (IBAction)sayIt:(id)sender;
--- SpeakLine/SpeakLIneAppDelegate.m
+++ SpeakLine/SpeakLIneAppDelegate.m
@@ -23,6 +23,9 @@
         // Create a new instance of NSSpeechSynthesizer
         // with the default voice.
         _speechSynth = [[NSSpeechSynthesizer alloc] initWithVoice:nil];
+        _voices = [NSSpeechSynthesizer availableVoices];
+        
+        [_speechSynth setDelegate:self];
     }
     return self;
 }
@@ -40,6 +43,61 @@
         return; }
     [_speechSynth startSpeakingString:string];
     NSLog(@"Have started to say: %@", string);
+    
+    [_stopButton setEnabled:true];
+    [_speakButton setEnabled:false];
+    [_tableView setEnabled:false];
+    
+}
+
+- (void)speechSynthesizer:(NSSpeechSynthesizer *)sender
+        didFinishSpeaking:(BOOL)finishedSpeaking
+{
+    [_speakButton setEnabled:true];
+    [_stopButton setEnabled:false];
+    [_tableView setEnabled:true];
+    NSLog(@"finishedSpeaking = %d", finishedSpeaking);
+}
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tv
+{
+    return (NSInteger)[_voices count];
+}
+
+- (id)tableView:(NSTableView *)tv objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row
+{
+    NSString *v = [_voices objectAtIndex:row];
+    NSDictionary *dict = [NSSpeechSynthesizer attributesForVoice:v];
+    //NSInteger row = [_tableView selectedRow];
+    return [dict objectForKey:NSVoiceName];
+}
+
+- (void)tableViewSelectionDidChange:(NSNotification *)notification
+{
+    NSInteger row = [_tableView selectedRow];
+    if (row == -1)
+    {
+        return;
+    }
+    NSString *selectedVoice = [_voices objectAtIndex:row];
+    [_speechSynth setVoice:selectedVoice];
+    NSLog(@"new voice = %@", selectedVoice);
+}
+
+- (void)awakeFromNib
+{
+    // When the table view appears on screen, the default voice should be selected
+    NSInteger defaultRow = [_voices indexOfObject:[NSSpeechSynthesizer defaultVoice]];
+    NSIndexSet *indices = [NSIndexSet indexSetWithIndex:defaultRow];
+    [_tableView selectRowIndexes:indices byExtendingSelection:NO];
+    [_tableView scrollRowToVisible:defaultRow];
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector
+{
+    NSString *methodName = NSStringFromSelector(aSelector);
+    NSLog(@"respondsToSelector:%@", methodName);
+    return [super respondsToSelector:aSelector];
 }
 
 @end
